415b79f141afa51c827bb56f66ad19ce87c91889
JacobItopa <sanijacobs@gmail.com>
2022-09-17 23:32:06 +0100

    Views

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, PostView, Like, Comments
 
 # Create your views here.
+
+class PostListView(generic.ListView):
+    model = Post
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug'
+    )
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = (
+        'title',
+        'content',
+        'thumbnail',
+        'author',
+        'slug'
+    )
+    
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    success_url = '/'
