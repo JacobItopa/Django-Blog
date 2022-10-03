91c20ccf6c24e2c1a2448aa33614479e72d14534
JacobItopa <sanijacobs@gmail.com>
2022-10-03 09:56:35 +0100

    likes and views

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from posts.views import PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
+from posts.views import PostListView, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView, like
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('<slug>/', PostDetailView.as_view(), name='detail'),
     path('<slug>/update/', PostUpdateView.as_view(), name='update'),
     path('<slug>/delete/', PostDeleteView.as_view(), name='delete'),
+    path('like/<slug>/', like, name='like')
     
 ]
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
-from .models import Post, PostView, Like, Comments
+from .models import Post, PostView, Like, Comments, User
 from .forms import PostForm
 
 # Create your views here.
@@ -10,6 +10,12 @@ class PostListView(generic.ListView):
 
 class PostDetailView(generic.DetailView):
     model = Post
+
+    def get_object(self, **kwargs):
+        object = super().get_object(**kwargs)
+        if self.request.user.is_authenticated:
+            PostView.objects.get_or_create(user=self.request.user, post=object)
+        return object
 
 class PostCreateView(generic.CreateView):
     model = Post
@@ -42,3 +48,14 @@ class PostUpdateView(generic.UpdateView):
 class PostDeleteView(generic.DeleteView):
     model = Post
     success_url = '/'
+
+    
+
+def like(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    like_qs = Like.objects.filter(user=request.user, post=post)
+    if like_qs.exists():
+        like_qs[0].delete()
+        return redirect('detail', slug=slug)
+    Like.objects.create(user=request.user, post=post)
+    return redirect('detail', slug=slug)
